9587a5822e79a4f83a92335e251c0ab01364dd0d
iman tung <iman.t@go-jek.com>
2018-02-28 06:38:25 +0700

    refactor run function

--- receiver/context.go
+++ receiver/context.go
@@ -5,19 +5,19 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/imantung/go-boilerplate/http/heartbeat"
+	"github.com/imantung/go-boilerplate/httpkit"
+	"github.com/imantung/go-boilerplate/httpkit/heartbeat"
 )
 
 // Context of receiver part
 type Context interface {
 	Init(config app.Configuration) (err error)
 	Run() (err error)
-	Configuration() Configuration
 }
 
 // receiver implementation
 type context struct {
-	conf Configuration
+	server httpkit.Server
 }
 
 // NewLogstoreReceiver
@@ -27,31 +27,31 @@ func NewContext() Context {
 
 // Init
 func (c *context) Init(config app.Configuration) (err error) {
-	c.conf = config.(Configuration)
+	conf := config.(Configuration)
 
+	c.server = &http.Server{
+		Addr:    conf.addr,
+		Handler: c.router(),
+	}
 	return
 }
 
 // Run
 func (c *context) Run() (err error) {
+	err = c.server.ListenAndServe()
+	return
+}
 
-	router := mux.NewRouter()
-	router.HandleFunc("/produce", c.ProduceHandler).Methods("POST")
+func (c *context) router() (router *mux.Router) {
+	router = mux.NewRouter()
+	router.HandleFunc("/produce", c.produceHandler).Methods("POST")
 	router.HandleFunc("/check-health", heartbeat.Handler)
-
-	err = http.ListenAndServe(c.conf.addr, router)
 
 	return
 }
 
-// Configuration
-func (c *context) Configuration() Configuration {
-	return c.conf
-
-}
-
 // ProducerHandler
-func (c *context) ProduceHandler(writer http.ResponseWriter, req *http.Request) {
+func (c *context) produceHandler(writer http.ResponseWriter, req *http.Request) {
 	// body, _ := ioutil.ReadAll(req.Body)
 	// TODO: parsing url to get group_id/store_id/service_id
 
--- receiver/context_test.go
+++ receiver/context_test.go
@@ -6,22 +6,49 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/imantung/go-boilerplate/errkit"
+	"github.com/imantung/go-boilerplate/httpkit"
 	"github.com/imantung/go-boilerplate/testkit"
 )
 
-func TestContext_Init(t *testing.T) {
+func TestNewContext(t *testing.T) {
 	ctx := NewContext()
-
-	// init
-	conf := Configuration{addr: ":8888"}
-	err := ctx.Init(conf)
-	testkit.FatalIfError(t, err)
 	testkit.FatalIf(
 		t,
-		ctx.Configuration() != conf,
-		"conf should be initiate",
+		ctx == nil,
+		"ctx should be initiate",
+	)
+}
+
+func TestContext_Init(t *testing.T) {
+	ctx := context{}
+
+	err := ctx.Init(Configuration{addr: ":8888"})
+	testkit.FatalIfError(t, err)
+
+	server := ctx.server.(*http.Server)
+	testkit.FatalIf(
+		t,
+		server.Addr != ":8888",
+		"Addr should be :8888",
 	)
 
+	testkit.FatalIf(
+		t,
+		server.Handler != nil,
+		"Handler should be initiate",
+	)
+}
+
+func TestContext_Run(t *testing.T) {
+	ctx := context{
+		server: httpkit.DummyServer{
+			ErrListAndServer: errkit.Error("error1"),
+		},
+	}
+
+	err := ctx.Run()
+	testkit.FatalIfWrongError(t, err, "error1")
 }
 
 func TestContext_ProduceHandler(t *testing.T) {
@@ -35,7 +62,6 @@ func TestContext_ProduceHandler(t *testing.T) {
 	)
 	rec := httptest.NewRecorder()
 
-	http.HandlerFunc(ctx.ProduceHandler).ServeHTTP(rec, req)
+	http.HandlerFunc(ctx.produceHandler).ServeHTTP(rec, req)
 	testkit.FatalIfWrongHttpCode(t, rec, http.StatusOK)
-
 }
