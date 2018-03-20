924d048ecd1995534866630e45177cf5db04403c
iman tung <iman.t@go-jek.com>
2018-03-20 14:21:56 +0700

    fix typo and testing for setErrorChannel

--- river/upstream_kafka_test.go
+++ river/upstream_kafka_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/BaritoLog/go-boilerplate/timekit"
 )
 
-func TestKafkaUpstream_WrongParameter(t *testing.T) {
+func TestKafkaUpstream_New_WrongParameter(t *testing.T) {
 	_, err := NewKafkaUpstream("meh")
 	FatalIfWrongError(t, err, "Parameter must be KafkaUpstreamConfig")
 }
 
-func TestKafkaUPstream_StartTransport_WrongBroker(t *testing.T) {
+func TestKafkaUpstream_StartTransport_WrongBroker(t *testing.T) {
 	conf := KafkaUpstreamConfig{
 		Brokers:         []string{"wrong_broker"},
 		ConsumerGroupId: "some-consumer-group",
@@ -23,10 +23,18 @@ func TestKafkaUPstream_StartTransport_WrongBroker(t *testing.T) {
 	FatalIfError(t, err)
 
 	go kafka.StartTransport()
-	timekit.Sleep("100ms")
+	timekit.Sleep("1ms")
 
 	err = <-kafka.ErrorChannel()
 
 	FatalIfWrongError(t, err, "kafka: client has run out of available brokers to talk to (Is your cluster reachable?)")
 
+}
+
+func TestKafkaUpstream_SetErrorChannel(t *testing.T) {
+	errCh := make(chan error)
+	upstream := kafkaUpstream{}
+	upstream.SetErrorChannel(errCh)
+
+	FatalIf(t, errCh != upstream.ErrorChannel(), "SetErrorChannel is not working")
 }
