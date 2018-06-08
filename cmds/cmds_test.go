517458a5f28381d852fc06b07ec3d2393573d893
Iman Tung <iman.t@go-jek.com>
2018-06-08 10:56:02 +0700

    unit testing GetKafkaBroker from env

--- cmds/cmds_test.go
+++ cmds/cmds_test.go
@@ -17,6 +17,14 @@ func TestGetKafkaBrokers_NoConsulAndNoEnv(t *testing.T) {
 	}
 }
 
+func TestGetKafkaBrokers_FromEnv(t *testing.T) {
+	os.Setenv(EnvKafkaBrokers, "kafka-broker-1:1278,kafka-broker-2:1288")
+	brokers := GetKafkaBrokers()
+	FatalIf(t, len(brokers) != 2, "wrong brokers")
+	FatalIf(t, brokers[0] != "kafka-broker-1:1278", "wrong brokers[0]")
+	FatalIf(t, brokers[1] != "kafka-broker-2:1288", "wrong brokers[1]")
+}
+
 func TestGetKafkaBorkersFromConsul_NoEnv(t *testing.T) {
 	_, err := getKafkaBrokerFromConsul()
 	FatalIfWrongError(t, err, "no ENV BARITO_CONSUL_URL")
