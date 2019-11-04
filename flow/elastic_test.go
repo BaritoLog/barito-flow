b303ca4373d77d7dad6b8930937150735486ec65
Daniel <nieltansah@gmail.com>
2019-11-04 14:21:09 +0700

    Add test for ES log stored metrics on consumer
    Co-authored-by: Beni <gnu.iben@gmail.com>

--- flow/elastic_test.go
+++ flow/elastic_test.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
 	. "github.com/BaritoLog/go-boilerplate/testkit"
 	"github.com/BaritoLog/instru"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	pb "github.com/vwidjaya/barito-proto/producer"
 )
 
@@ -82,4 +85,70 @@ func TestElasticStoreman_store_SaveError(t *testing.T) {
 	err = client.Store(context.Background(), timber)
 	FatalIfWrongError(t, err, "elastic: Error 400 (Bad Request)")
 	FatalIf(t, instru.GetEventCount(fmt.Sprintf("%s_es_store", appSecret), "fail") != 1, "wrong total fail event")
+}
+
+func TestElasticStore_ExportMetrics(t *testing.T) {
+	defer instru.Flush()
+
+	timber := *pb.SampleTimberProto()
+
+	ts := httptest.NewServer(&ELasticTestHandler{
+		ExistAPIStatus:  http.StatusOK,
+		CreateAPIStatus: http.StatusOK,
+		PostAPIStatus:   http.StatusOK,
+		ResponseBody: []byte(`
+			{
+				"took":2,
+				"errors":true,
+				"items":[{
+					"index":{
+						"_index":"index1",
+						"_type":"tweet",
+						"_id":"1",
+						"_version":3,
+						"status":200
+					}
+				},{
+					"index":{
+						"_index":"index1",
+						"_type":"tweet",
+						"_id":"2",
+						"_version":3,
+						"status":200
+					}
+				},{
+					"delete":{
+						"_index":"index2",
+						"_type":"tweet",
+						"_id":"1",
+						"_version":4,
+						"status":400,
+						"found":true,
+						"error": {
+							"type": "type",
+							"reason": "reason"
+						}
+					}
+				}]
+			}
+		`),
+	})
+	defer ts.Close()
+
+	retrier := mockElasticRetrier()
+	esConfig := NewEsConfig("BulkProcessor", 100, time.Duration(1000), false)
+	client, err := NewElastic(retrier, esConfig, []string{ts.URL})
+	FatalIfError(t, err)
+
+	err = client.Store(context.Background(), timber)
+	FatalIfError(t, err)
+
+	time.Sleep(2 * time.Second)
+	expected := `
+		# HELP barito_consumer_log_stored_total Number log stored to ES
+		# TYPE barito_consumer_log_stored_total counter
+		barito_consumer_log_stored_total{error="",index="index1",result="200",status=""} 2
+		barito_consumer_log_stored_total{error="reason",index="index2",result="400",status=""} 1
+	`
+	FatalIfError(t, testutil.GatherAndCompare(prometheus.DefaultGatherer, strings.NewReader(expected), "barito_consumer_log_stored_total"))
 }
--- flow/elastic_test_handler.go
+++ flow/elastic_test_handler.go
@@ -6,6 +6,15 @@ type ELasticTestHandler struct {
 	ExistAPIStatus  int
 	CreateAPIStatus int
 	PostAPIStatus   int
+	ResponseBody    []byte
+}
+
+func (handler *ELasticTestHandler) getResponseBody() (body []byte) {
+	body = handler.ResponseBody
+	if body == nil {
+		body = []byte(`{}`)
+	}
+	return
 }
 
 func (handler *ELasticTestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +22,9 @@ func (handler *ELasticTestHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 		w.WriteHeader(handler.ExistAPIStatus)
 	} else if r.Method == "PUT" { // create index
 		w.WriteHeader(handler.CreateAPIStatus)
-		w.Write([]byte(`{}`))
+		w.Write(handler.getResponseBody())
 	} else if r.Method == "POST" { // post message
 		w.WriteHeader(handler.PostAPIStatus)
-		w.Write([]byte(`{}`))
+		w.Write(handler.getResponseBody())
 	}
 }
