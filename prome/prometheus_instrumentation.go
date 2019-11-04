ff5bca9258099e20b1e3d7ffbfcdcb39ef9ce857
Daniel <nieltansah@gmail.com>
2019-11-04 14:37:28 +0700

    Reformat prometheus_instrumentation.go
    Co-authored-by: Beni <gnu.iben@gmail.com>

--- prome/prometheus_instrumentation.go
+++ prome/prometheus_instrumentation.go
@@ -1,9 +1,10 @@
 package prome
 
 import (
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"strconv"
 )
 
 var consumerLogStoredCounter *prometheus.CounterVec
@@ -13,57 +14,56 @@ var producerKafkaMessageStoredTotal *prometheus.CounterVec
 var producerHttpRequestTotal *prometheus.CounterVec
 var producerHttpRequestTime *prometheus.SummaryVec
 
-
-func InitConsumerInstrumentation()  {
+func InitConsumerInstrumentation() {
 	consumerLogStoredCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name:    "barito_consumer_log_stored_total",
-		Help:    "Number log stored to ES",
+		Name: "barito_consumer_log_stored_total",
+		Help: "Number log stored to ES",
 	}, []string{"index", "status", "result", "error"})
 	consumerBulkProcessTimeSecond = promauto.NewSummary(prometheus.SummaryOpts{
-		Name:    "barito_consumer_bulk_process_time_second",
-		Help:    "Bulk process time in second ",
+		Name:       "barito_consumer_bulk_process_time_second",
+		Help:       "Bulk process time in second ",
 		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 	})
 	consumerKafkaMessagesIncomingCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name:    "barito_consumer_kafka_message_incoming_total",
-		Help:    "Number of messages incoming from kafka",
+		Name: "barito_consumer_kafka_message_incoming_total",
+		Help: "Number of messages incoming from kafka",
 	}, []string{"topic"})
 }
 
-func InitProducerInstrumentation()  {
+func InitProducerInstrumentation() {
 	producerKafkaMessageStoredTotal = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name:    "barito_producer_kafka_message_stored_total",
-		Help:    "Number of message stored to kafka",
+		Name: "barito_producer_kafka_message_stored_total",
+		Help: "Number of message stored to kafka",
 	}, []string{"topic", "is_error"})
 	producerHttpRequestTotal = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name:    "barito_producer_http_request_total",
-		Help:    "Number of incoming http request",
+		Name: "barito_producer_http_request_total",
+		Help: "Number of incoming http request",
 	}, []string{"status", "path", "method"})
 	producerHttpRequestTime = promauto.NewSummaryVec(prometheus.SummaryOpts{
-		Name:    "barito_producer_http_request_second",
-		Help:    "Summary time incomding http request",
+		Name:       "barito_producer_http_request_second",
+		Help:       "Summary time incomding http request",
 		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 	}, []string{"status", "path", "method"})
 }
 
-func IncreaseLogStoredCounter(index string, result string, status int, error string)  {
-    consumerLogStoredCounter.WithLabelValues(index, result, strconv.Itoa(status), error).Inc()
+func IncreaseLogStoredCounter(index string, result string, status int, error string) {
+	consumerLogStoredCounter.WithLabelValues(index, result, strconv.Itoa(status), error).Inc()
 }
 
-func IncreaseKafkaMessagesIncoming(topic string)  {
+func IncreaseKafkaMessagesIncoming(topic string) {
 	consumerKafkaMessagesIncomingCounter.WithLabelValues(topic).Inc()
 }
 
-func ObserveBulkProcessTime(elapsedTime float64 )  {
-    consumerBulkProcessTimeSecond.Observe(elapsedTime)
+func ObserveBulkProcessTime(elapsedTime float64) {
+	consumerBulkProcessTimeSecond.Observe(elapsedTime)
 }
 
-func IncreaseKafkaMessagesStoredTotal(topic string, isError string)  {
+func IncreaseKafkaMessagesStoredTotal(topic string, isError string) {
 	producerKafkaMessageStoredTotal.WithLabelValues(topic, isError).Inc()
 }
-func IncreaseProducerHttpRequestTotal(status int, path string, method string)  {
+func IncreaseProducerHttpRequestTotal(status int, path string, method string) {
 	producerHttpRequestTotal.WithLabelValues(strconv.Itoa(status), path, method).Inc()
 }
-func ObserveProducerHttpRequestTime(elapsedTime float64, status int, path string, method string)  {
+func ObserveProducerHttpRequestTime(elapsedTime float64, status int, path string, method string) {
 	producerHttpRequestTime.WithLabelValues(strconv.Itoa(status), path, method).Observe(elapsedTime)
 }
