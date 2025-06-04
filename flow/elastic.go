6ed7b38c26915524f4930ac6ae75ad2f926bc663
bentol <bentol@users.noreply.github.com>
2025-06-04 07:06:45 +0700

    feat(datastream): add datastream as option to index document to ES (#101)
    * feat(consumer): add ability to push to datastream using op_type create
    
    * feat(consumer): add ability to push to datastream using op_type create, create new index template automatically
    
    * feat(consumer): add ability to push to datastream using op_type create, make the default component template configurable
    
    * feat(consumer): add ability to push to datastream using op_type create, remove debug line
    
    ---------
    
    Co-authored-by: Beni Budiharto <beni.harmadi@gojek.com>

--- flow/elastic.go
+++ flow/elastic.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/BaritoLog/barito-flow/prome"
 
@@ -12,7 +11,7 @@ import (
 
 	pb "github.com/bentol/barito-proto/producer"
 	"github.com/golang/protobuf/jsonpb"
-	"github.com/olivere/elastic"
+	"github.com/olivere/elastic/v7"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/zekroTJA/timedmap"
@@ -22,6 +21,10 @@ var counter int = 0
 
 const (
 	DEFAULT_ELASTIC_DOCUMENT_TYPE = "_doc"
+
+	IndexMethodBulkProcessor = "BulkProcessor"
+	IndexMethodDatastream    = "DataStream"
+	IndexMethodSingleInsert  = "SingleInsert"
 )
 
 type Elastic interface {
@@ -31,12 +34,14 @@ type Elastic interface {
 }
 
 type elasticClient struct {
-	client           *elastic.Client
-	bulkProcessor    *elastic.BulkProcessor
-	onFailureFunc    func(*pb.Timber)
-	onStoreFunc      func(ctx context.Context, indexName, documentType, document string) (err error)
-	jspbMarshaler    *jsonpb.Marshaler
-	indexExistsCache *timedmap.TimedMap
+	client                             *elastic.Client
+	bulkProcessor                      *elastic.BulkProcessor
+	onFailureFunc                      func(*pb.Timber)
+	onStoreFunc                        func(ctx context.Context, indexName, documentType, document string) (err error)
+	jspbMarshaler                      *jsonpb.Marshaler
+	indexExistsCache                   *timedmap.TimedMap
+	useDataStream                      bool
+	dataStreamDefaultComponentTemplate string
 
 	redactor Redactor
 }
@@ -52,19 +57,21 @@ func (r *DummyRedactor) Redact(indexName, document string) (string, error) {
 }
 
 type esConfig struct {
-	indexMethod string
-	bulkSize    int
-	flushMs     time.Duration
-	printTPS    bool
+	indexMethod                        string
+	bulkSize                           int
+	flushMs                            time.Duration
+	printTPS                           bool
+	dataStreamDefaultComponentTemplate string
 }
 
-func NewEsConfig(indexMethod string, bulkSize int, flushMs time.Duration, printTPS bool) esConfig {
+func NewEsConfig(indexMethod string, bulkSize int, flushMs time.Duration, printTPS bool, dataStreamDefaultComponentTemplate string) esConfig {
 
 	return esConfig{
-		indexMethod: indexMethod,
-		bulkSize:    bulkSize,
-		flushMs:     flushMs,
-		printTPS:    printTPS,
+		indexMethod:                        indexMethod,
+		bulkSize:                           bulkSize,
+		flushMs:                            flushMs,
+		printTPS:                           printTPS,
+		dataStreamDefaultComponentTemplate: dataStreamDefaultComponentTemplate,
 	}
 }
 
@@ -101,16 +108,21 @@ func NewElastic(retrierFunc *ElasticRetrier, esConfig esConfig, urls []string, e
 	}
 
 	client = elasticClient{
-		client:           c,
-		bulkProcessor:    p,
-		jspbMarshaler:    &jsonpb.Marshaler{},
-		indexExistsCache: timedmap.New(1 * time.Minute),
-		redactor:         &DummyRedactor{},
+		client:                             c,
+		bulkProcessor:                      p,
+		jspbMarshaler:                      &jsonpb.Marshaler{},
+		indexExistsCache:                   timedmap.New(1 * time.Minute),
+		redactor:                           &DummyRedactor{},
+		useDataStream:                      false,
+		dataStreamDefaultComponentTemplate: esConfig.dataStreamDefaultComponentTemplate,
 	}
 
-	if esConfig.indexMethod == "BulkProcessor" {
+	if esConfig.indexMethod == IndexMethodBulkProcessor {
 		client.onStoreFunc = client.bulkInsert
-	} else if esConfig.indexMethod == "SingleInsert" {
+	} else if esConfig.indexMethod == IndexMethodDatastream {
+		client.onStoreFunc = client.bulkInsertDataStream
+		client.useDataStream = true
+	} else if esConfig.indexMethod == IndexMethodSingleInsert {
 		client.onStoreFunc = client.singleInsert
 	}
 
@@ -159,24 +171,73 @@ func (e *elasticClient) WithRedactor(r Redactor) *elasticClient {
 	return e
 }
 
+func (e *elasticClient) ensureIndexIsExistsRegularIndex(ctx context.Context, indexName string) bool {
+	log.Warnf("ES index '%s' is not exist", indexName)
+	_, err := e.client.CreateIndex(indexName).Do(ctx)
+	instruESCreateIndex(err)
+	if err != nil {
+		log.Errorf("Error creating index: %s", err)
+		return false
+	}
+	e.indexExistsCache.Set(indexName, true, 1*time.Minute)
+	return true
+}
+
+func (e *elasticClient) ensureIndexIsExistsDataStream(ctx context.Context, datastreamName string) bool {
+	log.Warnf("ES datastream index '%s' is not exist", datastreamName)
+	payload := fmt.Sprintf(
+		`{"index_patterns":["%s"],"data_stream":{},"composed_of":["%s"],"priority":200,"_meta":{"description":"default template"}} `,
+		datastreamName,
+		e.dataStreamDefaultComponentTemplate,
+	)
+	_, err := e.client.IndexPutIndexTemplate(datastreamName).
+		Name(datastreamName + "-template").
+		BodyString(payload).
+		Do(ctx)
+
+	if err != nil {
+		log.Errorf("Error creating index template %s: %s", datastreamName, err)
+		return false
+	}
+
+	return true
+}
+
+func (e *elasticClient) ensureIndexIsExists(ctx context.Context, indexName string) bool {
+	indexCacheFound := e.indexExistsCache.GetValue(indexName)
+	if indexCacheFound != nil {
+		return true
+	}
+
+	exists, err := e.client.IndexExists(indexName).Do(ctx)
+	if err != nil {
+		log.Errorf("Error checking if index exists: %s", err)
+		return false
+	}
+
+	if !exists {
+		if e.useDataStream {
+			return e.ensureIndexIsExistsDataStream(ctx, indexName)
+		}
+		return e.ensureIndexIsExistsRegularIndex(ctx, indexName)
+	}
+	return true
+}
+
 func (e *elasticClient) Store(ctx context.Context, timber pb.Timber) (err error) {
 	indexPrefix := timber.GetContext().GetEsIndexPrefix()
 	indexName := fmt.Sprintf("%s-%s", indexPrefix, time.Now().Format("2006.01.02"))
+	if e.useDataStream {
+		indexName = indexPrefix
+	}
 	documentType := DEFAULT_ELASTIC_DOCUMENT_TYPE
 	appSecret := timber.GetContext().GetAppSecret()
 
-	indexCacheFound := e.indexExistsCache.GetValue(indexName)
-	if indexCacheFound == nil {
-		exists, _ := e.client.IndexExists(indexName).Do(ctx)
-		if !exists {
-			log.Warnf("ES index '%s' is not exist", indexName)
-			_, err = e.client.CreateIndex(indexName).Do(ctx)
-			instruESCreateIndex(err)
-			if (err != nil) && (!strings.Contains(err.Error(), "resource_already_exists_exception")) {
-				return
-			}
+	// ensure index is exists before push the logs
+	for {
+		if e.ensureIndexIsExists(ctx, indexName) {
+			break
 		}
-		e.indexExistsCache.Set(indexName, true, 1*time.Minute)
 	}
 
 	document, err := ConvertTimberToEsDocumentString(timber, e.jspbMarshaler)
@@ -204,6 +265,16 @@ func (e *elasticClient) OnFailure(f func(*pb.Timber)) {
 
 func (e *elasticClient) bulkInsert(_ context.Context, indexName, documentType, document string) (err error) {
 	r := elastic.NewBulkIndexRequest().
+		Index(indexName).
+		Type(documentType).
+		Doc(document)
+	e.bulkProcessor.Add(r)
+	return
+}
+
+func (e *elasticClient) bulkInsertDataStream(_ context.Context, indexName, documentType, document string) (err error) {
+	r := elastic.NewBulkIndexRequest().
+		OpType("create").
 		Index(indexName).
 		Type(documentType).
 		Doc(document)
